90eba690fa350ee7bca1e3711943affed647f126
brandonolin <olin.brandon@gmail.com>
2020-09-14 13:49:56 -0600

    Added functionality to generate jwt token and assign to user account on successful account creation

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema(
   {
@@ -30,12 +31,28 @@ const userSchema = new mongoose.Schema(
           throw new Error("Password cannot contain the word 'password'!")
         }
       }
-    }
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true
+        }
+      }
+    ]
   },
   {
     timestamps: true
   }
 )
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+}
 
 userSchema.pre('save', async function (next) {
   const user = this;
